db4c6f258980e2014ba20ad428ff1f29818348a2
rhj <aha199277@qq.com>
2015-12-14 22:30:21 +0800

    0.0.6

--- OneChat/app/src/main/java/tk/imrhj/onechat/Fragment/ConversationFragment.java
+++ OneChat/app/src/main/java/tk/imrhj/onechat/Fragment/ConversationFragment.java
@@ -1,9 +1,22 @@
 package tk.imrhj.onechat.Fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
 import tk.imrhj.onechat.R;
 
 public class ConversationFragment extends Fragment {
+    private ListView chatListView;
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_conversation, container, false);
+
+    }
 }
