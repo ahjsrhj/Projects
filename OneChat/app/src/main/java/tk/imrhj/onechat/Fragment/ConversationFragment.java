247b186f2eb0b45e71e5158c8c0765d07d63bd4c
rhj <aha199277@qq.com>
2015-12-14 22:37:04 +0800

    0.0.7

--- OneChat/app/src/main/java/tk/imrhj/onechat/Fragment/ConversationFragment.java
+++ OneChat/app/src/main/java/tk/imrhj/onechat/Fragment/ConversationFragment.java
@@ -8,15 +8,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.avoscloud.leanchatlib.utils.ConversationManager;
+
 import tk.imrhj.onechat.R;
 
 public class ConversationFragment extends Fragment {
-    private ListView chatListView;
+    private ListView mChatListView;
+    private ConversationManager conversationManager;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_conversation, container, false);
+        mChatListView = (ListView) view.findViewById(R.id.lv_chat_list);
+        mChatListView.setEmptyView(view.findViewById(R.id.tv_chat_list_empty_view));
 
+        conversationManager = ConversationManager.getInstance();
+
+
+        return view;
     }
 }
