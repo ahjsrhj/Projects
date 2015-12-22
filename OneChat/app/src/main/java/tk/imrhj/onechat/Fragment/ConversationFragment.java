ad899d0a398dbb7aa6edc5a6a9f0ad4b15c12fe6
rhj <aha199277@qq.com>
2015-12-22 20:42:07 +0800

    0.0.13

--- OneChat/app/src/main/java/tk/imrhj/onechat/Fragment/ConversationFragment.java
+++ OneChat/app/src/main/java/tk/imrhj/onechat/Fragment/ConversationFragment.java
@@ -7,14 +7,23 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.im.v2.AVIMClient;
 import com.avos.avoscloud.im.v2.AVIMConversation;
+import com.avos.avoscloud.im.v2.AVIMException;
+import com.avos.avoscloud.im.v2.AVIMMessage;
+import com.avos.avoscloud.im.v2.Conversation;
+import com.avos.avoscloud.im.v2.callback.AVIMSingleMessageQueryCallback;
+import com.avoscloud.leanchatlib.event.ImTypeMessageEvent;
 import com.avoscloud.leanchatlib.model.Room;
 import com.avoscloud.leanchatlib.utils.ConversationManager;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import de.greenrobot.event.EventBus;
@@ -25,12 +34,11 @@ import tk.imrhj.onechat.R;
 /**
  * Created by rhj on 15/12/16.
  */
-public class ConversationFragment extends Fragment {
+public class ConversationFragment extends Fragment implements AdapterView.OnItemClickListener {
     private static final String TAG = "ConversationFragment";
     private ListView mChatList;
-    private AVIMClient mClient;
     private List<AVIMConversation> mConversationList;
-    private List<UserListAdapter.User> mUserList;
+    private List<UserListAdapter.User> mRoomList;
     private UserListAdapter mAdapter;
     private ConversationManager mConversationManager;
 
@@ -43,11 +51,19 @@ public class ConversationFragment extends Fragment {
         mConversationManager = ConversationManager.getInstance();
         EventBus.getDefault().register(this);
         //首先为ListView设置默认的Adapter
-//        initListView();
+        initListView();
 
-        //为ListView添加用户数据
-//        initUserList();
         return view;
+    }
+
+    /**
+     * 初始化ListView
+     */
+    private void initListView() {
+        mRoomList = new ArrayList<>();
+        mAdapter = new UserListAdapter(getActivity(), mRoomList);
+        mChatList.setAdapter(mAdapter);
+        mChatList.setOnItemClickListener(this);
     }
 
     @Override
@@ -77,6 +93,10 @@ public class ConversationFragment extends Fragment {
             public void done(List<Room> roomList, AVException exception) {
                 if (null == exception) {
                     Log.d(TAG, "done: " + roomList.size());
+                    List<Room> sortedRooms = sortRooms(roomList);
+                    updateLastMessage(sortedRooms);
+                    addToRoomList(sortedRooms);
+
                 } else {
                     Log.e(TAG, "done: " + exception.getMessage());
                 }
@@ -85,7 +105,86 @@ public class ConversationFragment extends Fragment {
 
     }
 
+    private void addToRoomList(List<Room> roomList) {
+        for (Room room: roomList) {
+            UserListAdapter.User user = new UserListAdapter.User();
+            user.mUserID = room.getConversation().getMembers().toString();
+            Log.d(TAG, "addToRoomList: " + user.mUserID);
+
+        }
+    }
+
+    /**
+     * 更新消息
+     * @param roomList
+     */
+    private void updateLastMessage(final List<Room> roomList) {
+        for (final Room room : roomList) {
+            AVIMConversation conversation = room.getConversation();
+            if (null != conversation) {
+                conversation.getLastMessage(new AVIMSingleMessageQueryCallback() {
+                    @Override
+                    public void done(AVIMMessage avimMessage, AVIMException e) {
+                        if (null != e && null != avimMessage) {
+                            room.setLastMessage(avimMessage);
+                        }
+                    }
+                });
+            }
+        }
+
+    }
+
+
+    /**
+     * 给房间按时间排序
+     * @param roomList
+     * @return
+     */
+    private List<Room> sortRooms(final List<Room> roomList) {
+        List<Room> sortedList = new ArrayList<Room>();
+        if (null != roomList) {
+            sortedList.addAll(roomList);
+            Collections.sort(sortedList, new Comparator<Room>() {
+                @Override
+                public int compare(Room lhs, Room rhs) {
+                    long value = lhs.getLastModifyTime() - rhs.getLastModifyTime();
+                    if (value > 0) {
+                        return -1;
+                    } else if (value < 0) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                }
+            });
+        }
+        return sortedList;
+    }
+
+    public void onEvent(ImTypeMessageEvent event) {
+        updateConversationList();
+    }
+
     public void onEvent(ConversationFragmentUpdateEvent updateEvent) {
         updateConversationList();
+    }
+
+    /**
+     * Callback method to be invoked when an item in this AdapterView has
+     * been clicked.
+     * <p>
+     * Implementers can call getItemAtPosition(position) if they need
+     * to access the data associated with the selected item.
+     *
+     * @param parent   The AdapterView where the click happened.
+     * @param view     The view within the AdapterView that was clicked (this
+     *                 will be a view provided by the adapter)
+     * @param position The position of the view in the adapter.
+     * @param id       The row id of the item that was clicked.
+     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
